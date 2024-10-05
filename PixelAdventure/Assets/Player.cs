8d813367f71a37037ed06531e51aac3762c93a57
duxuantunglam <duxuantunglam2k2@gmail.com>
2024-10-05 14:56:46 +0700

    f*ck Gismoz

--- PixelAdventure/Assets/Player.cs
+++ PixelAdventure/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,14 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private float xInput;
 
@@ -20,10 +28,30 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
-        
+        HandleCollision();
+        HandleInput();
         HandleAnimations();
         HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void HandleAnimations() {
@@ -32,5 +60,10 @@ public class Player : MonoBehaviour
 
     private void HandleMovement() {
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
