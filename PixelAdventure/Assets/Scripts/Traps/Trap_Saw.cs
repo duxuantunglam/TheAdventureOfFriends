d6d1975de73904495f8d099c3700392e2aad4b7d
duxuantunglam <duxuantunglam2k2@gmail.com>
2024-10-26 14:51:20 +0700

    trap_fire and fix trap_saw --> complete traps

--- PixelAdventure/Assets/Scripts/Traps/Trap_Saw.cs
+++ PixelAdventure/Assets/Scripts/Traps/Trap_Saw.cs
@@ -10,11 +10,10 @@ public class Trap_Saw : MonoBehaviour
     [SerializeField] private float moveSpeed = 3;
     [SerializeField] private float cooldown = 1;
     [SerializeField] private Transform[] wayPoint;
-
-    [SerializeField] private Vector3[] wayPointPosition;
+    private Vector3[] wayPointPosition;
 
     public int wayPointIndex = 1;
-    private int moveDirection = 1;
+    public int moveDirection = 1;
     private bool canMove = true;
 
     private void Awake()
@@ -25,12 +24,24 @@ public class Trap_Saw : MonoBehaviour
 
     private void Start()
     {
-        UpdateWayPointsInfo();
+        UpdateWaypointsInfo();
         transform.position = wayPointPosition[0];
     }
 
-    private void UpdateWayPointsInfo()
+    private void UpdateWaypointsInfo()
     {
+        List<Trap_SawWaypoint> wayPointList = new List<Trap_SawWaypoint>(GetComponentsInChildren<Trap_SawWaypoint>());
+
+        if (wayPointList.Count != wayPoint.Length)
+        {
+            wayPoint = new Transform[wayPointList.Count];
+
+            for (int i = 0; i < wayPointList.Count; i++)
+            {
+                wayPoint[i] = wayPointList[i].transform;
+            }
+        }
+
         wayPointPosition = new Vector3[wayPoint.Length];
 
         for (int i = 0; i < wayPoint.Length; i++)
@@ -56,7 +67,7 @@ public class Trap_Saw : MonoBehaviour
                 StartCoroutine(StopMovement(cooldown));
             }
 
-                wayPointIndex = wayPointIndex + moveDirection;         
+            wayPointIndex = wayPointIndex + moveDirection;
         }
     }
 
