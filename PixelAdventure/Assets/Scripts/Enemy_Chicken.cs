dd28967c034eb76f63812d7e4c64c9f56457ebfd
duxuantunglam <duxuantunglam2k2@gmail.com>
2024-11-05 04:22:53 +0700

    enemyRino & fix enemyChicken

--- PixelAdventure/Assets/Scripts/Enemy_Chicken.cs
+++ PixelAdventure/Assets/Scripts/Enemy_Chicken.cs
@@ -75,4 +75,18 @@ public class Enemy_Chicken : Enemy
         base.Flip();
         canFlip = true;
     }
+
+    protected override void HandleCollision()
+    {
+        base.HandleCollision();
+
+        playerDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, detectionRange, whatIsPlayer);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (detectionRange * facingDir), transform.position.y));
+    }
 }
