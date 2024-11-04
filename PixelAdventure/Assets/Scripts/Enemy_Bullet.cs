9b425cbd5acb320b70578925ec53c445b0804345
duxuantunglam <duxuantunglam2k2@gmail.com>
2024-11-05 06:31:19 +0700

    enemyPlant with Bullet

---
+++ PixelAdventure/Assets/Scripts/Enemy_Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Bullet : MonoBehaviour
+{
+    [SerializeField] private string playerLayerName = "Player";
+    [SerializeField] private string groundLayerName = "Ground";
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void FlipSprite() => sr.flipX = !sr.flipX;
+
+    public void SetVelocity(Vector2 velocity) => rb.velocity = velocity;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer(playerLayerName))
+        {
+            collision.GetComponent<Player>().KnockBack(transform.position.x);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer(groundLayerName))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
