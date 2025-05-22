9fcb15a571d0c6cae0b6421cf42fd60af880afe2
duxuantunglam <duxuantunglam2k2@gmail.com>
2025-05-22 23:53:06 +0700

    update user data

--- TheAdventureOfFriends/Assets/Scripts/Firebase/UserData.cs
+++ TheAdventureOfFriends/Assets/Scripts/Firebase/UserData.cs
@@ -1,8 +1,35 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LevelStats
+{
+    public float bestTime = 999f;
+    public int bestFruitCollected = 0;
+    public bool unlocked = false;
+}
 
 [Serializable]
 public class UserData
 {
     public string userName;
     public string id;
+
+    public int totalFruitAmount = 0;
+    public int continueLevelNumber = 1;
+    public int lastUsedSkin = 0;
+    public float averageBestTime = 0f;
+    public int gameDifficulty = 1;
+
+    public Dictionary<string, LevelStats> levelProgress = new Dictionary<string, LevelStats>();
+
+    public Dictionary<string, bool> skinUnlockedStatus = new Dictionary<string, bool>();
+
+    public UserData()
+    {
+        levelProgress["Level1"] = new LevelStats { unlocked = true };
+
+        skinUnlockedStatus["DefaultSkinName"] = true;
+    }
 }
