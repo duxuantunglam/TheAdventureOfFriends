f492825599f80bb60eedc063e2ffa0f1923839bb
duxuantunglam <duxuantunglam2k2@gmail.com>
2025-05-26 10:04:08 +0700

    UI noti

---
+++ TheAdventureOfFriends/Assets/Scripts/Firebase/UI_NotificationInGame.cs
@@ -0,0 +1,75 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_NotificationInGame : MonoBehaviour
+{
+    [SerializeField] private GameObject notificationPanel;
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private TMP_Text messageText;
+    [SerializeField] private Button acceptButton;
+    [SerializeField] private Button declineButton;
+    [SerializeField] private Button closeButton;
+
+    public void ShowNotification(string title, string message, Action onAccept, Action onDecline, Action onClose)
+    {
+        if (notificationPanel != null)
+        {
+            notificationPanel.SetActive(true);
+
+            if (titleText != null) titleText.text = title;
+            if (messageText != null) messageText.text = message;
+
+            if (acceptButton != null) acceptButton.onClick.RemoveAllListeners();
+            if (declineButton != null) declineButton.onClick.RemoveAllListeners();
+            if (closeButton != null) closeButton.onClick.RemoveAllListeners();
+
+            if (acceptButton != null && onAccept != null)
+            {
+                acceptButton.onClick.AddListener(() =>
+                {
+                    onAccept.Invoke();
+                    HideNotification();
+                });
+            }
+
+            if (declineButton != null && onDecline != null)
+            {
+                declineButton.onClick.AddListener(() =>
+                {
+                    onDecline.Invoke();
+                    HideNotification();
+                });
+            }
+
+            if (closeButton != null && onClose != null)
+            {
+                closeButton.onClick.AddListener(() =>
+                {
+                    onClose.Invoke();
+                    HideNotification();
+                });
+            }
+            else if (closeButton != null)
+            {
+                closeButton.onClick.AddListener(HideNotification);
+            }
+            if (acceptButton != null && onAccept == null) acceptButton.onClick.AddListener(HideNotification);
+            if (declineButton != null && onDecline == null) declineButton.onClick.AddListener(HideNotification);
+
+        }
+    }
+
+    public void HideNotification()
+    {
+        if (notificationPanel != null)
+        {
+            notificationPanel.SetActive(false);
+
+            if (acceptButton != null) acceptButton.onClick.RemoveAllListeners();
+            if (declineButton != null) declineButton.onClick.RemoveAllListeners();
+            if (closeButton != null) closeButton.onClick.RemoveAllListeners();
+        }
+    }
+}
